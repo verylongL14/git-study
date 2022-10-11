94759e73306a3edebc7ad5322d90e196784c850c
MJL14 <3460125400@qq.com>
2022-10-11 17:30:06 +0800

    修改了Hello类

--- src/main/java/com/software/contorller/Hello.java
+++ src/main/java/com/software/contorller/Hello.java
@@ -3,5 +3,7 @@ package com.software.contorller;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World1!");
+        System.out.println("Hello World2!");
     }
 }
